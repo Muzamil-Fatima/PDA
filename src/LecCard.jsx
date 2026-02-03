4c1d9597cf0c7b57d7f43c80e780eea114add84c
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2026-02-03 15:57:55 +0500

    Add file

---
+++ src/LecCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LecCard = () => {
+  return (
+    <div>LecCard</div>
+  )
+}
+
+export default LecCard
---
+++ src/LecList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LecList = () => {
+  return (
+    <div>LecList</div>
+  )
+}
+
+export default LecList
